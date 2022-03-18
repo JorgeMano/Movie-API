443cbbbfc2bac2e1aa6554da9e233ffe7afcf132
JorgeMano <jorge.l.manosalva@gmail.com>
2022-03-17 22:40:56 -0500

    Movie functions defined

--- backend/controller/movies.controller.js
+++ backend/controller/movies.controller.js
@@ -13,19 +13,115 @@ const { filterObj } = require('../util/filterObject');
 dotenv.config({ path: './config.env' });
 
 exports.getAllMovies = catchAsync(async (req, res, next) => {
-    
+    const users = await Movie.findAll({
+        where: { status: 'active' },
+        include: [
+            {
+                model: Movie,
+                include: [
+                    {
+                        model: Review,
+                        include: [
+                            {
+                                model: User,
+                                attributes: { exclude: ['password'] }
+                            }
+                        ]
+                    }
+                ]
+            },
+            { model: Review, include: [{ model: Movie }] }
+        ]
+    });
+    res.status(200).json({
+        status: 'success',
+        data: { movies }
+    });
 });
 
 exports.getMovieById = catchAsync(async (req, res, next) => {
-    
+    const { id } = req.params;
+    const movie = await Movie.findOne({ where: { id } });
+
+    if(!movie){
+        return next(new AppError(404, 'Movie not found'));
+    }
+    res.status(200).json({
+        status: 'success',
+        data: {
+            movie
+        }
+    });
 });
 
 exports.createNewMovie = catchAsync(async (req, res, next) => {
-    
+    const { title, description, duration, rating, img, genre } = req.body;
+    if(!title || !description || !duration || !rating ||!img ||!genre) {
+        return next(new AppError(400, 'Must provide a valid, title, description, duration, rating, img, genre'));
+    };
+    const newMovie = await Movie.create({
+        title, 
+        description,
+        duration,
+        rating,
+        img,
+        genre
+    });
+    res.status(200).json({
+        status: 'success',
+        data: {
+            newMovie
+        }
+    });
 });
 
 exports.updateMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const data = filterObj(
+            req.body,
+            'title',
+            'description',
+            'duration',
+            'genre'
+        );
+        const movie = await Movie.findOne({
+            where: { id: id, status: 'active' }
+        });
+        if(!movie) {
+            res.status(404).json({
+                status: 'error',
+                message: 'Cant update movie, invalid ID'
+            });
+            return;
+        }
+        await movie.update({ ...data });
+        res.status(204).json({
+            status: 'success'
+        });
+    }  catch (error) {
+        console.log(error);       
+    }
 };
 
 exports.deleteMovie = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const movie = await Movie.findOne({
+            where: {id: id, status: 'active'}
+        });
+        if(!movie){
+            res.status(404).json({
+                status: 'error',
+                message: 'Cant delete movie, invalid ID'
+            });
+            return;
+        }
+        await movie.update({ status: 'deleted' });
+        res.status(204).json({
+            status: 'success'
+        });
+    } catch (error) {
+        console.log(error);
+    }
 };
